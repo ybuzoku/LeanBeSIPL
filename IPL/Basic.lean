f17b51bb7f69b34ed1fb883c05ce0b2df5659696
ybuzoku <ybuzoku@hotmail.co.uk>
2024-11-26 23:31:43 +0000

    szuruburu part tworu

--- IPL/Basic.lean
+++ IPL/Basic.lean
@@ -22,26 +22,37 @@ structure Rule where
   conc : Nat
 
 inductive AtDer : (Set Rule × Finset Nat × Nat) -> Prop where
-  | Ref (B : Set Rule) (S : Finset Nat) (p : Nat) : p ∈ S → AtDer (B, S, p)
-  | App (B : Set Rule) (S : Finset Nat) (p : Nat) (R : Rule) : (R ∈ B) → (R.conc = p) → (∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc)) → AtDer (B, S, p)
+  | Ref (B : Set Rule) (L : Finset Nat) (p : Nat) : p ∈ L → AtDer (B, L, p)
+  | App (B : Set Rule) (L : Finset Nat) (p : Nat) (R : Rule) : R ∈ B → R.conc = p → ∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ L, seq.conc) → AtDer (B, L, p)
 
-theorem lem21 (B : Set Rule) (S L : Finset Nat) (p : Nat) : AtDer (B, S, p) -> AtDer (B, S∪L, p) := by
+theorem lem21 (B : Set Rule) (P U : Finset Nat) (q : Nat) : AtDer (B, P, q) -> AtDer (B, P ∪ U, q) := by
   intro x
   cases x with
-  | Ref hB hS hp hpInS =>
+  | Ref hB hP hq hqInP =>
   apply AtDer.Ref
   simp --Turns membership of union into a disjunction of memberships
   left
-  exact hpInS
-  | App hB hS hp hR hRInhB hRconcIshp iDerivations =>
+  exact hqInP
+  | App hB hP hq hR ruleInBase ruleConcIsOk aseqFromRule aseqFromRuleInRule atDerForSeq =>
   apply AtDer.App
-  . exact hRInhB
-  . exact hRconcIshp
-  . intro ruleAtSeq
-    specialize iDerivations ruleAtSeq
-    intro ruleAtSeqInRule
-    constructor
-    simp
+  . exact ruleInBase
+  . exact ruleConcIsOk
+  . refine aseqFromRuleInRule
+  cases atDerForSeq with
+  | Ref _ _ _ hqInHypoU  =>
+  apply AtDer.Ref
+  sorry
+  | App _ _ _ _ => sorry
+
+
+
+
+
+
+
+
+
+
 
 theorem lem22 (B : Set Rule) (S L : Finset Nat) (p : Nat) :
   AtDer (B, S, p) <-> ∀C ⊇ B, ∀ s ∈ S, AtDer (C, ∅, s) -> AtDer (C, ∅, p) := by
