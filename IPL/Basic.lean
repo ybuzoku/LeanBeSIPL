9ecbbfac13a3121385b6b86ac763ed4d559007b1
ybuzoku <ybuzoku@hotmail.co.uk>
2024-11-26 16:03:38 +0000

    Updating

--- IPL/Basic.lean
+++ IPL/Basic.lean
@@ -1,10 +1,16 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Finset.Basic
 
-set_option diagnostics true
-
 /-
 structure Atom : Type
+-/
+
+--Define Atoms as a natural number like construction
+
+/-
+inductive Atom
+  | zero : Atom
+  | succ (a : Atom) : Atom
 -/
 
 structure AtSeq where
@@ -14,22 +20,33 @@ structure AtSeq where
 structure Rule where
   seq : Finset AtSeq
   conc : Nat
-/-
-structure Base where
-  rules : Set Rule
--/
-
-/-
-inductive AtDer : (Base × Finset Nat × Nat) -> Prop where
-  | Ref (B : Base) (S : Finset Nat) (p : Nat) : p ∈ S → AtDer (B, S, p)
-  | App (B : Base) (S : Finset Nat) (p : Nat) (R : Rule) : R ∈ B.rules → R.conc = p → ∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc) → AtDer (B, S, p)
--/
 
 inductive AtDer : (Set Rule × Finset Nat × Nat) -> Prop where
   | Ref (B : Set Rule) (S : Finset Nat) (p : Nat) : p ∈ S → AtDer (B, S, p)
-  | App (B : Set Rule) (S : Finset Nat) (p : Nat) (R : Rule) : R ∈ B → R.conc = p → ∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc) → AtDer (B, S, p)
+  | App (B : Set Rule) (S : Finset Nat) (p : Nat) (R : Rule) : (R ∈ B) → (R.conc = p) → (∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc)) → AtDer (B, S, p)
 
-theorem lem21 (B : Set Rule) (S L : Finset Nat) (p : Nat) : AtDer (B, S, p) -> AtDer (B, S∪L, p) := sorry
+theorem lem21 (B : Set Rule) (S L : Finset Nat) (p : Nat) : AtDer (B, S, p) -> AtDer (B, S∪L, p) := by
+  intro x
+  cases x with
+  | Ref hB hS hp hpInS =>
+  apply AtDer.Ref
+  simp --Turns membership of union into a disjunction of memberships
+  left
+  exact hpInS
+  | App hB hS hp hR hRInhB hRconcIshp iDerivations =>
+  apply AtDer.App
+  . exact hRInhB
+  . exact hRconcIshp
+  . intro ruleAtSeq
+    specialize iDerivations ruleAtSeq
+    intro ruleAtSeqInRule
+    constructor
+    simp
 
 theorem lem22 (B : Set Rule) (S L : Finset Nat) (p : Nat) :
-  AtDer (B, S, p) <-> ∀C ⊇ B, ∀ s ∈ S, AtDer (C, ∅, s) -> AtDer (C, ∅, p) := sorry
+  AtDer (B, S, p) <-> ∀C ⊇ B, ∀ s ∈ S, AtDer (C, ∅, s) -> AtDer (C, ∅, p) := by
+  apply Iff.intro
+  . intro h
+    sorry
+  . intro h
+    sorry
