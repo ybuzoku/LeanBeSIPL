ab9788439c066e3f673c9fc440154930561dcb70
ybuzoku <ybuzoku@hotmail.co.uk>
2024-11-28 01:23:18 +0000

    Getting closer I think

--- IPL/Basic.lean
+++ IPL/Basic.lean
@@ -23,43 +23,8 @@ structure Rule where
 
 inductive AtDer : (Set Rule × Finset Nat × Nat) -> Prop where
   | Ref (B : Set Rule) (S : Finset Nat) (p : Nat) : p ∈ S → AtDer (B, S, p)
-  | App (B : Set Rule) (S : Finset Nat) (r : Nat) (R : Rule) : R ∈ B → ∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc) → AtDer (B, S, R.conc)
+  | App (B : Set Rule) (S : Finset Nat) (r : Nat) (R : Rule) : R ∈ B → (R.conc = r) -> (∀ seq ∈ R.seq, AtDer (B, seq.hypo ∪ S, seq.conc)) → AtDer (B, S, r)
 
-
-theorem lem21 (B : Set Rule) (P U : Finset Nat) (q : Nat) : AtDer (B, P, q) -> AtDer (B, P ∪ U, q) := by
-  intro hAtDer
-  generalize hArgs : (B, P, q) = args
-  rw [hArgs] at hAtDer
-  induction hAtDer with
-  | Ref hB hP hq hqInP =>
-    apply AtDer.Ref
-    simp
-    simp at hArgs
-    obtain ⟨hBisB, t⟩ := hArgs
-    obtain ⟨hPisP, hqIsq⟩ := t
-    left
-    rw [<-hPisP] at hqInP
-    rw [<-hqIsq] at hqInP
-    exact hqInP
-  | App hB hP hq hR hRinhB hAtseq hAtseqInR hDer i =>
-    sorry
-/-
-example (B : Set Rule) (P U : Finset Nat) (q : Nat) : AtDer (B, P, q) -> AtDer (B, P ∪ U, q) := by
-  intro hAtDer
-  generalize hArgs : (B, P, q) = args
-  simp [hArgs] at *
-  induction hAtDer
-  case Ref hB hP hq hqInP =>
-    apply AtDer.Ref
-    simp
-    left
-
-
-  case App hB hP hq hR hRinhB hAtseq hAtseqInR hDer' i =>
-    apply i
--/
-
-/-
 theorem lem21 (B : Set Rule) (P U : Finset Nat) (q : Nat) : AtDer (B, P, q) -> AtDer (B, P ∪ U, q) := by
   intro hAtDer
   generalize hArgs : (B, P, q) = args
@@ -75,54 +40,15 @@ theorem lem21 (B : Set Rule) (P U : Finset Nat) (q : Nat) : AtDer (B, P, q) -> A
     rw [<-hPisP] at hqInhP
     rw [<-hqIsq] at hqInhP
     exact hqInhP
-  | App hB hP hq hR hRinhB hRConcIshq hAtSeq hAtSeqInRule hDer i =>
+  | App hB hP hr hR hRinhB hConcIshr hAders i =>
     simp at hArgs
     obtain ⟨hBisB, t⟩ := hArgs
     obtain ⟨hPisP, hqIsq⟩ := t
     apply AtDer.App
-    . rw [<-hBisB] at hRinhB
-      refine hRinhB
-    . rw [<-hqIsq] at hRConcIshq
-      exact hRConcIshq
-    . exact hAtSeqInRule
-    . rw [<-hBisB] at i
-      rw [<-hBisB] at hDer
-      simp at i
-      induction hDer with
-      | Ref _ _ _ hConcInSeqHypo =>
-        apply AtDer.Ref
-        rw [<-hPisP] at hConcInSeqHypo
-        simp
-        simp at hConcInSeqHypo
-        cases hConcInSeqHypo with
-        | inl h =>
-          left
-          exact h
-        | inr h =>
-          right
-          left
-          exact h
-      | App ihB ihP ihq ihR ihRinihB ihRConcIsihq ihAtSeq ihAtSeqInRule ihDer =>
--/
-
-      /-
-        apply AtDer.App
-        . exact ihRinihB
-        . exact ihRConcIsihq
-        . exact ihAtSeqInRule
-        .
-      -/
-
-
-
-
-
-
-
-
-
-
-
+    . sorry
+    . sorry
+    . sorry
+    . exact hR
 
 
 theorem lem22 (B : Set Rule) (S L : Finset Nat) (p : Nat) :
